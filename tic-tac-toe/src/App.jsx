c4c43b1531008603e23ed5a2f3a73db66d1f3d8f
JuanDavidSerna <jserna@cloudlabs.co>
2023-08-23 15:02:23 -0500

    JuegoConReact

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,31 @@
+const TURNS = {
+  X : 'x',
+  O :'o'
+}
+
+const board = Array(9).fill(null)
+
+function App() {
+  return (
+    <main className='board'>
+      <h1>Tic Tac Toe</h1>
+      <section className='game'>
+        {
+          board.map((_, index) => {
+              return (
+                <div className='cell' key={index}>
+                  <span className='cell_content'>
+                    {index}
+                  </span>
+                </div>
+
+              )
+            }) 
+        }
+      </section>
+    </main>
+  )
+  
+  
+}
+export default App
